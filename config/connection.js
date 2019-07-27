d92896f2f61f94ef939072b9db00ac4a878778e2
Daniel Jackson <officialdanieljackson@gmail.com>
2019-07-27 10:27:38 -0700

    fixed port code on heroku

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "",
+        password: "Dodgers1",
         database: "burgers_db"
     });
 };
